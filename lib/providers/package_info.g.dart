a1b93f0f8f46fc1ecbe1f2cc33b5bbc130cff73b
Varun Narravula <varun@snare.dev>
2024-09-18 04:49:48 -0700

    feat: add version number to home screen

---
+++ lib/providers/package_info.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'package_info.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$packageInfoHash() => r'70b4a9f2a022e5b2378e51d1d49d1f88448f9784';
+
+/// See also [packageInfo].
+@ProviderFor(packageInfo)
+final packageInfoProvider = AutoDisposeFutureProvider<PackageInfo>.internal(
+  packageInfo,
+  name: r'packageInfoProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$packageInfoHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PackageInfoRef = AutoDisposeFutureProviderRef<PackageInfo>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
