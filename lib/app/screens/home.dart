7f42ec11ac96b923ec36c807256e483faeded9f6
Varun Narravula <varun@snare.dev>
2024-09-18 05:37:17 -0700

    feat: app init, part 2 (#2)
    This brings in a bunch of fixes for stuff that I messed up, as well as
    some other QoL niceties.
    
    Summary:
    - Fastlane configuration for faster deployment (who woulda thunk it?)
    - VSCode launch config with --dart-define env
    - Icon and name fixes
    - Version number on home screen for debugging purposes
    - Basic docs!

--- lib/app/screens/home.dart
+++ lib/app/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:chai/models/models.dart';
+import 'package:chai/providers/package_info.dart';
 import 'package:chai/repository/hello.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,26 +20,55 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Chai Solutions'),
       ),
       body: Center(
-        child: Consumer(builder: (context, ref, child) {
-          final AsyncValue<HelloResponse> res = ref.watch(getHelloProvider);
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer(
+              builder: (context, ref, child) {
+                final AsyncValue<HelloResponse> res =
+                    ref.watch(getHelloProvider);
 
-          return switch (res) {
-            AsyncData(:final value) => Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    value.message,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
+                return switch (res) {
+                  AsyncData(:final value) => Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          value.message,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text('Your random number is: ${value.number}'),
+                      ],
                     ),
-                  ),
-                  Text('Your random number is: ${value.number}'),
-                ],
-              ),
-            AsyncError() => const Text('something went wrong'),
-            _ => const CircularProgressIndicator(),
-          };
-        }),
+                  AsyncError() => const Text('something went wrong'),
+                  _ => const CircularProgressIndicator(),
+                };
+              },
+            ),
+            Consumer(
+              builder: (context, ref, child) {
+                final AsyncValue<PackageInfo> packageInfo =
+                    ref.watch(packageInfoProvider);
+
+                return switch (packageInfo) {
+                  AsyncData(:final value) => Column(
+                      children: [
+                        const SizedBox(height: 10),
+                        Text(
+                          'Version: ${value.version}+${value.buildNumber}',
+                          style: const TextStyle(
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    ),
+                  _ => const SizedBox.shrink(),
+                };
+              },
+            )
+          ],
+        ),
       ),
       floatingActionButton: Consumer(
         builder: (context, ref, child) {
