9f30717a3d4a2497a9cbdbda6e7eed67cb3d867b
Varun Narravula <varun@snare.dev>
2024-11-05 13:10:35 -0800

    feat(notifications): add onesignal user and request permissions

---
+++ lib/providers/onesignal.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'onesignal.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$oneSignalServiceHash() => r'6b8e54dd3d9309147874496eaf35fa7658ef5db2';
+
+/// See also [oneSignalService].
+@ProviderFor(oneSignalService)
+final oneSignalServiceProvider = AutoDisposeProvider<OneSignalService>.internal(
+  oneSignalService,
+  name: r'oneSignalServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$oneSignalServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef OneSignalServiceRef = AutoDisposeProviderRef<OneSignalService>;
+String _$oneSignalListenerHash() => r'658d780c5aa44dbde91aaa9c3192abea00850131';
+
+/// See also [oneSignalListener].
+@ProviderFor(oneSignalListener)
+final oneSignalListenerProvider = AutoDisposeProvider<void>.internal(
+  oneSignalListener,
+  name: r'oneSignalListenerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$oneSignalListenerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef OneSignalListenerRef = AutoDisposeProviderRef<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
