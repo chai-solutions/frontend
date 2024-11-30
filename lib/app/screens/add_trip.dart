002beab40525a875db68e87e7ea48937eefa1093
thamizarasus <thamizarasus@gmail.com>
2024-11-29 18:26:51 -0800

    fixing back arrow and adding ALS on assets folder

--- lib/app/screens/add_trip.dart
+++ lib/app/screens/add_trip.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
 class AddTripPage extends StatefulWidget {
@@ -15,20 +16,12 @@ class _AddTripPageState extends State<AddTripPage> {
   final _flightNumberController = TextEditingController();
   DateTime? _selectedDate;
 
-  List<Map<String, TextEditingController>> _additionalFlights = [];
-
   @override
   void dispose() {
     _departureController.dispose();
     _arrivalController.dispose();
     _airlineController.dispose();
     _flightNumberController.dispose();
-    for (var flight in _additionalFlights) {
-      flight['departure']?.dispose();
-      flight['arrival']?.dispose();
-      flight['airline']?.dispose();
-      flight['flightNumber']?.dispose();
-    }
     super.dispose();
   }
 
@@ -46,27 +39,6 @@ class _AddTripPageState extends State<AddTripPage> {
     }
   }
 
-  void _addFlightSection() {
-    setState(() {
-      _additionalFlights.add({
-        'departure': TextEditingController(),
-        'arrival': TextEditingController(),
-        'airline': TextEditingController(),
-        'flightNumber': TextEditingController(),
-      });
-    });
-  }
-
-  void _removeFlightSection(int index) {
-    setState(() {
-      _additionalFlights[index]['departure']?.dispose();
-      _additionalFlights[index]['arrival']?.dispose();
-      _additionalFlights[index]['airline']?.dispose();
-      _additionalFlights[index]['flightNumber']?.dispose();
-      _additionalFlights.removeAt(index);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -74,7 +46,11 @@ class _AddTripPageState extends State<AddTripPage> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context);
+            if (GoRouter.of(context).canPop()) {
+              GoRouter.of(context).pop();
+            } else {
+              context.go('/'); // Fallback to homepage
+            }
           },
         ),
         actions: [
@@ -156,97 +132,12 @@ class _AddTripPageState extends State<AddTripPage> {
                   ),
                 ],
               ),
-              const Divider(thickness: 1.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    'Do you have another flight?',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      if (_additionalFlights.isNotEmpty)
-                        IconButton(
-                          icon: const Icon(Icons.remove_circle,
-                              color: Colors.red),
-                          onPressed: () => _removeFlightSection(
-                              _additionalFlights.length - 1),
-                        ),
-                      IconButton(
-                        icon: const Icon(Icons.add_circle, color: Colors.green),
-                        onPressed: _addFlightSection,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              for (int i = 0; i < _additionalFlights.length; i++)
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10.0),
-                    Text(
-                      'Flight ${i + 2}',
-                      style: const TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purpleAccent, // Light purple color
-                      ),
-                    ),
-                    const SizedBox(height: 10.0),
-                    TextField(
-                      controller: _additionalFlights[i]['departure'],
-                      decoration: const InputDecoration(
-                        labelText: 'Departure Airport',
-                        prefixIcon: Icon(Icons.flight_takeoff),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextField(
-                      controller: _additionalFlights[i]['arrival'],
-                      decoration: const InputDecoration(
-                        labelText: 'Arrival Airport',
-                        prefixIcon: Icon(Icons.flight_land),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextField(
-                      controller: _additionalFlights[i]['airline'],
-                      decoration: const InputDecoration(
-                        labelText: 'Airline',
-                        prefixIcon: Icon(Icons.airplanemode_active),
-                      ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextField(
-                      controller: _additionalFlights[i]['flightNumber'],
-                      decoration: const InputDecoration(
-                        labelText: 'Flight Number',
-                        prefixIcon: Icon(Icons.confirmation_number),
-                      ),
-                    ),
-                    const Divider(thickness: 1.0),
-                  ],
-                ),
               const SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: () {
-                      // Save logic
-                    },
-                    child: const Text('Save'),
-                  ),
-                ],
+              ElevatedButton(
+                onPressed: () {
+                  // Save logic
+                },
+                child: const Text('Save'),
               ),
             ],
           ),
