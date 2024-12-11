353155ba7e55b9189a1eb07fd4b585cae0571b00
Arjun Bhagat <rjunbhagat@gmail.com>
2024-12-11 15:11:56 -0800

    remove bruh

--- lib/app/screens/notifications.dart
+++ lib/app/screens/notifications.dart
@@ -2,7 +2,6 @@ import 'package:chai/providers/http_client.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Notifications extends ConsumerStatefulWidget {
