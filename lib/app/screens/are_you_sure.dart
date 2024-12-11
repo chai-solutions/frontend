761843b4dd2410dbb385b03fe490cb2c6498235f
thamizarasus <thamizarasus@gmail.com>
2024-12-10 22:24:09 -0800

    cleaner UI

--- lib/app/screens/are_you_sure.dart
+++ lib/app/screens/are_you_sure.dart
@@ -10,92 +10,71 @@ class AreYouSure extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 75.0,
-            ),
-            //button to go back home
-            child: ElevatedButton.icon(
-              onPressed: () async {
+          // Left arrow button at the top-left corner
+          Positioned(
+            top: 70,
+            left: 20,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
                 if (context.mounted) {
                   context.go('/home');
                 }
               },
-              icon: const Icon(Icons.arrow_back),
-              label: const Text('Back Home'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.white, // Set the background color to white
-              ),
             ),
           ),
-          LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              double maxWidth = constraints.maxWidth * 0.7;
-              return SizedBox(
-                width: maxWidth,
-                child: const Text(
-                  'Are You Sure You Want To Create A New Flight Plan?',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
+          // Main content of the screen
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    double maxWidth = constraints.maxWidth * 0.7;
+                    return SizedBox(
+                      width: maxWidth,
+                      child: const Text(
+                        'Are You Sure You Want To Create A New Flight Plan?',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center, // Center the text
+                      ),
+                    );
+                  },
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    if (context.mounted) {
+                      context.go('/searchFirstHome');
+                    }
+                  },
+                  label: const Text('YES'),
+                  icon: const Icon(Icons.check_circle),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
                   ),
-                  textAlign: TextAlign.center, // Optional: Center the text
                 ),
-              );
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 75.0,
-            ),
-            //button to go back home
-            child: ElevatedButton.icon(
-              onPressed: () async {
-                if (context.mounted) {
-                  context.go('/searchFirstHome');
-                }
-              },
-              label: const Text('YES'),
-              icon: const Icon(Icons.check_circle),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.white, // Set the background color to white
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 0.0,
-            ),
-            //button to go back home
-            child: ElevatedButton.icon(
-              onPressed: () async {
-                if (context.mounted) {
-                  context.go('/home');
-                }
-              },
-              label: const Text('NO'),
-              icon: const Icon(Icons.cancel),
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    Colors.white, // Set the background color to white
-              ),
-            ),
-          ),
-          const Align(
-              alignment: Alignment.center,
-              child: Padding(
-                padding: EdgeInsets.only(bottom: 400.0),
-                child: Column(
-                  children: [
-                    Padding(padding: EdgeInsets.all(0.0)),
-                  ],
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () async {
+                    if (context.mounted) {
+                      context.go('/home');
+                    }
+                  },
+                  label: const Text('NO'),
+                  icon: const Icon(Icons.cancel),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
                 ),
-              )),
+              ],
+            ),
+          ),
         ],
       ),
     );
